75b97038805724a5aa9a647f114454e9abd91ca3
Jirka Hladky <jhladky@redhat.com>
2022-11-19 02:54:42 +0100

    Fixed ./results/measure_runtime.sh

--- results/measure_runtime.sh
+++ results/measure_runtime.sh
@@ -2,11 +2,11 @@
 
 #parse_log $file $name
 #Looks for Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): in $file and stores the value to results.txt in format
-#"${name} \t\t${runtime}
+#"${name} \t\t\t${runtime}
 parse_log() {
   runtime=$(grep -Poh "Elapsed \(wall clock\) time \(h:mm:ss or m:ss\):\s+\K[0-9.:]+" "$1")
   if [ -n "$runtime" ]; then
-    echo -e "${2} \t\t${runtime}" > results.txt
+    echo -e "${2} \t\t\t${runtime}" | tee --append results.txt
   else
     echo "WARN: '${1}' - could not parse 'Elapsed (wall clock) time' value"
   fi
@@ -16,7 +16,7 @@ cpu_name=$(lscpu | grep -Poh "^\s*Model name:\s+\K.*$" | sed -e 's/(.*)//g' | tr
 [ -d "$cpu_name" ] && rm -rf "./${cpu_name}"
 mkdir "./$cpu_name"
 pushd "./$cpu_name" || exit 1
-echo -e "Test \tWall clock (m:ss)" > results.txt
+echo -e "Test \t\t\tWall clock (m:ss)" > results.txt
 
 while IFS= read -r -d '' bin; do
   #echo "$bin"
@@ -28,22 +28,27 @@ while IFS= read -r -d '' bin; do
     declare -A methods=([0]=HarmonicSeriesAVX512 [1]=HarmonicSeriesAVX256 [2]=HarmonicSeriesPlain)
     for method in "${!methods[@]}"; do
       name=${methods[$method]}
+      command=("turbostat" "-o" "${name}.turbostat" "/usr/bin/time" "-v" "perf" "stat" "${bin}" "$method" "4e9")
+      echo "${command[*]}"
       sleep 5
-      if /usr/bin/time -v "${bin}" "$method" 4e9  > "${name}.log" 2>&1; then
+      if "${command[@]}"  > "${name}.log" 2>&1; then
         parse_log "${name}.log" "$name"
       else
         echo "ERROR: ${bin_name} ${name} returned non-zero exit status"
       fi
     done
+    continue
   fi
 
+  command=("turbostat" "-o" "${bin_name}.turbostat" "/usr/bin/time" "-v" "perf" "stat" "${bin}")
+  echo "${command[*]}"
   sleep 5
-  if /usr/bin/time -v "${bin}" > "${bin_name}.log" 2>&1 ; then
+  if "${command[@]}" > "${bin_name}.log" 2>&1 ; then
     parse_log "${bin_name}.log" "${bin_name}"
   else
     echo "ERROR: ${bin_name} returned non-zero exit status"
   fi
-done < <(find ../../bin -maxdepth 1 -type f -executable -print0)
+done < <(find ../../bin -maxdepth 1 -type f -executable -print0 | sort -z)
 
 echo "=================================================================================="
 tail -v -n +1 results.txt
